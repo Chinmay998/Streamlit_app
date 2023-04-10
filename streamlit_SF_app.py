146c466b821e32099f74ef229f8086f87566779a
Chinmay998 <122522457+Chinmay998@users.noreply.github.com>
2023-04-10 15:37:41 +0530

    Update streamlit_SF_app.py

--- streamlit_SF_app.py
+++ streamlit_SF_app.py
@@ -5,7 +5,7 @@ conn = snowflake.connector.connect(**st.secrets["snowflake"])
 session = Session.builder.configs(conn).create()
 my_cur = my_cnx.cursor()
 def db_list():
-  dbs=session.sql("SHOW DATABASES ;").collect
+  dbs=session.sql("SHOW DATABASES ;").collect()
   db_list = [list(row.asDict().value()[1] for row in dbs]
    return db_list
 my_data_row = conn.fetchone()
