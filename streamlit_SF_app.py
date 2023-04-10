54dcfcc74085c00af8695961b3f092c7fa11524d
Chinmay998 <122522457+Chinmay998@users.noreply.github.com>
2023-04-10 15:52:58 +0530

    Update streamlit_SF_app.py

--- streamlit_SF_app.py
+++ streamlit_SF_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
+import snowflake.snowpark
 import snowflake.connector
 from snowflake.snowpark import Session 
 from snowflake.snowpark.functions import *
 conn = snowflake.connector.connect(**st.secrets["snowflake"])
 session = Session.builder.configs(conn).create()
-my_cur = my_cnx.cursor()
 def db_list():
   dbs=session.sql("SHOW DATABASES ;").collect()
   db_list = [list(row.asDict().value())[1] for row in dbs]
