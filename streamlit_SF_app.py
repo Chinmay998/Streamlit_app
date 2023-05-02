a801633db5bc6c4ab0d8a7060207a72f8ce1900d
Chinmay998 <122522457+Chinmay998@users.noreply.github.com>
2023-05-02 22:35:14 +0530

    Update streamlit_SF_app.py

--- streamlit_SF_app.py
+++ streamlit_SF_app.py
@@ -1,98 +1,87 @@
-from snowflake.snowpark.session import Session
-from snowflake.snowpark.functions import avg, sum, col,lit
-import streamlit as st
-import pandas as pd
-import configparser
 import os
+import pandas as pd 
+import snowflake.connector
+import streamlit as st 
+import configparser
+import numpy as np
+import time
 
-# # Page config must be set
+#provide header for the app
 st.set_page_config(
     layout="wide",
     page_title="Weather Report"
 )
 
-# Step 2 Create connection parameters
-#Configure config.ini file path
-
+#Set up config parser to parse config.ini file
 config = configparser.ConfigParser()
-ini_path = os.path.join(os.getcwd(),'config.ini')
-config.read(ini_path)
+config.sections()
+config.read('config.ini')
 
-# Setup config.ini read rules
+#Set up Snowflake login credential variables
 
 sfAccount = config['SnowflakePOC']['sfAccount']
 sfUser = config['SnowflakePOC']['sfUser']
 sfPass = config['SnowflakePOC']['sfPass']
-sfRole = config['SnowflakePOC']['sfRole']
 sfDB = config['SnowflakePOC']['sfDB']
 sfSchema = config['SnowflakePOC']['sfSchema']
 sfWarehouse = config['SnowflakePOC']['sfWarehouse']
-def create_session_object():
-        connection_parameters = {           "account":sfAccount,
-                                        "user": sfUser,
-                                        "password":sfPass,
-                                        "role":sfRole,
-                                        "warehouse":sfWarehouse,
-                                        "database":sfDB ,
-                                        "schema":sfSchema
-                                }
 
-        # # Step 3 Create a session using the connection parameters
-        session = Session.builder.configs(connection_parameters).create()
-        print(session.sql('select current_warehouse(), current_database(), current_schema()').collect())
-        return session
+#Print SF user name to test login credential variables are setup properly
+print(sfUser)
+print(sfAccount)
 
+#Set up connection with Snowflake
+conn = snowflake.connector.connect(user = sfUser,
+                                    password = sfPass,
+                                    account = sfAccount,
+                                    warehouse = sfWarehouse,
+                                    database = sfDB,
+                                    schema = sfSchema
+                                    )
 
+cs = conn.cursor()
 
-def load_data(session):
-        df_history = session.table("CLIMATE.HISTORY_DAY").filter(col('COUNTRY') == 'US')
-        df_history = df_history.group_by('POSTAL_CODE').max('MAX_TEMPERATURE_FEELSLIKE_2M_F').filter(col('POSTAL_CODE') != '445001').sort('MAX(MAX_TEMPERATURE_FEELSLIKE_2M_F)',ascending=False).limit(10).collect()
-        df_history= [list(row.asDict().values()) for row in df_history]
+cs.execute("USE ROLE ACCOUNTADMIN")
+
+# df_history = session.table("CLIMATE.HISTORY_DAY").filter(col('COUNTRY') == 'US').select(col('POSTAL_CODE'),col('MAX_TEMPERATURE_FEELSLIKE_2M_F'))
+# df_history = df_history.group_by('POSTAL_CODE').max('MAX_TEMPERATURE_FEELSLIKE_2M_F').filter(col('POSTAL_CODE') != '445001').filter(col('DATE_VALID_STD')<= '2023-04-18').sort('MAX(MAX_TEMPERATURE_FEELSLIKE_2M_F)',ascending=False).limit(10).collect()
+#         df_history= [list(row.asDict().values()) for row in df_history]
         
-        pd_df_history =pd.DataFrame(df_history,columns=['POSTAL_CODE','MAX_TEMPERATURE_FEELSLIKE_2M_F'])
+#         pd_df_history =pd.DataFrame(df_history,columns=['POSTAL_CODE','MAX_TEMPERATURE_FEELSLIKE_2M_F'])
 
-        
+df_history = pd.read_sql ("SELECT  POSTAL_CODE,MAX(MAX_TEMPERATURE_FEELSLIKE_2M_F)MAX_TEMPERATURE_FEELSLIKE_2M_F,DATE_VALID_STD FROM CLIMATE.HISTORY_DAY where COUNTRY = 'US' AND DATE_VALID_STD <= '2023-04-18' group by POSTAL_CODE,DATE_VALID_STD limit 10",conn)
+#st.write(df_history)
 
-
-
-       
-        df_forecast = session.table("CLIMATE.HISTORY_DAY").filter(col('COUNTRY') == 'US')
-        df_forecast = df_forecast.group_by('POSTAL_CODE').max('MAX_TEMPERATURE_FEELSLIKE_2M_F').filter(col('POSTAL_CODE') != '445001').sort('MAX(MAX_TEMPERATURE_FEELSLIKE_2M_F)',ascending=False).limit(10).collect()
-        df_forecast= [list(row.asDict().values()) for row in df_forecast]
-        pd_df_forecast =pd.DataFrame(df_forecast,columns=['POSTAL_CODE','MAX_TEMPERATURE_FEELSLIKE_2M_F'])
+df_forecast =pd.read_sql ("SELECT  POSTAL_CODE,MAX(MAX_TEMPERATURE_FEELSLIKE_2M_F)MAX_TEMPERATURE_FEELSLIKE_2M_F,DATE_VALID_STD FROM GLOBAL_WEATHER_REPORT.CLIMATE.FORCAST_DAY where COUNTRY = 'US' AND DATE_VALID_STD <= '2023-04-18' group by POSTAL_CODE,DATE_VALID_STD limit 10",conn)
+#st.write(df_forecast)
 
 
-       
-        st.header("Weather Sourc:Global Weather & Climate Data for BI")
-        st.subheader("Powered by Snowpark for Python and Snowflake Data Marketplace | Made with Streamlit")
+st.header("Weather Sourc:Global Weather & Climate Data for BI")
+st.subheader("Powered by Snowpark for Python and Snowflake Data Marketplace | Made with Streamlit")
 
         
 
-        col1, col2 = st.columns(2)
-        with st.container():
+col1, col2 = st.columns(2)
+with st.container():
                 with col1:
                         st.subheader(' Weather History- USA')
-                        st.dataframe(pd_df_history)
+                        st.dataframe(df_history)
                 with col2:
                         st.subheader('Weather Forecast- USA')
-                        st.dataframe(pd_df_forecast)
+                        st.dataframe(df_forecast)
         
                 
-       
-        with st.container():
-                st.subheader('TEMPERATURE')
+        #col3, col4 = st.columns(2)
+                with st.container():
+                 st.subheader('TEMPERATURE')
                 with st.expander(""):
-                
+                 #Set Threshold As the values in Table to view the graph
                  temp_threshold = st.number_input(label='Temperature Threshold',min_value=70, value=126, step=1)
-      
-        st.subheader('Weather History Barchart')
-        pd_df_history_top_n = pd_df_history[pd_df_history['MAX_TEMPERATURE_FEELSLIKE_2M_F']< temp_threshold][['POSTAL_CODE','MAX_TEMPERATURE_FEELSLIKE_2M_F']].astype({'MAX_TEMPERATURE_FEELSLIKE_2M_F': float}).reset_index(drop=True)
-        st.bar_chart(data=pd_df_history_top_n, width=850, height=500, use_container_width=True,x='POSTAL_CODE',y='MAX_TEMPERATURE_FEELSLIKE_2M_F')
-        
-        st.subheader('Weather Forecast Barchart')
-        pd_df_forecast_top_n = pd_df_forecast[pd_df_forecast['MAX_TEMPERATURE_FEELSLIKE_2M_F']< temp_threshold][['POSTAL_CODE','MAX_TEMPERATURE_FEELSLIKE_2M_F']].astype({'MAX_TEMPERATURE_FEELSLIKE_2M_F': float}).reset_index(drop=True)
-        st.bar_chart(data=pd_df_forecast_top_n, width=850, height=500, use_container_width=True,x='POSTAL_CODE',y='MAX_TEMPERATURE_FEELSLIKE_2M_F')
 
-if __name__ == "__main__":
-    session = create_session_object()
-    load_data(session)
+st.subheader('Weather History Barchart')
+pd_df_history_top_n = df_history[df_history['MAX_TEMPERATURE_FEELSLIKE_2M_F']< temp_threshold][['POSTAL_CODE','MAX_TEMPERATURE_FEELSLIKE_2M_F']].astype({'MAX_TEMPERATURE_FEELSLIKE_2M_F': float}).reset_index(drop=True)
+st.bar_chart(data=pd_df_history_top_n, width=850, height=500, use_container_width=True,x='POSTAL_CODE',y='MAX_TEMPERATURE_FEELSLIKE_2M_F')
+        
+st.subheader('Weather Forecast Barchart')
+pd_df_forecast_top_n = df_forecast[df_forecast['MAX_TEMPERATURE_FEELSLIKE_2M_F']< temp_threshold][['POSTAL_CODE','MAX_TEMPERATURE_FEELSLIKE_2M_F']].astype({'MAX_TEMPERATURE_FEELSLIKE_2M_F': float}).reset_index(drop=True)
+st.bar_chart(data=pd_df_forecast_top_n, width=850, height=500, use_container_width=True,x='POSTAL_CODE',y='MAX_TEMPERATURE_FEELSLIKE_2M_F')
